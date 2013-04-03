5bc6d34bd16aae36b07c0da4204a18ebdb5173e7
Andrew Richardson <andrewr114@gmail.com>
2013-04-02 22:23:44 -0700

    Initial Commit

---
+++ InstanceHook/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  InstanceHook
+//
+//  Created by Andrew Richardson on 2013-04-02.
+//  Copyright (c) 2013 Andrew Richardson. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	printf("Hello, World!\n");
+    return 0;
+}
+
